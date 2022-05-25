493c0140074298ed6867be9d8e3fa81740b6a25d
Luis Maurício <36276548+cpluis@users.noreply.github.com>
2022-05-24 22:03:43 -0300

    adicionando comentario linha 120

--- Servidor.java
+++ Servidor.java
@@ -116,6 +116,7 @@ public class Servidor extends Thread {
         /*Condicional que joga a mensagem escrita para o servidor*/
         for (BufferedWriter bw : clientes) {
             bwS = (BufferedWriter) bw;
+           //o que faz esse if
             if (!(bwSaida == bwS)) {
                 // É assim que ficara na tela
                 bw.write(nome + " -> " + msg + "\r\n");
